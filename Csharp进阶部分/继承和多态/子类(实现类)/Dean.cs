3150eadd15c8253c984cfe7949c1c9bd574d578f
Ambrose <1903569007@qq.com>
2025-01-02 14:04:08 +0800

    接口继承和多态

---
+++ Csharp进阶部分/继承和多态/子类(实现类)/Dean.cs
@@ -0,0 +1,21 @@
+﻿using Csharp进阶部分.继承和多态.接口;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp进阶部分.继承和多态.子类_实现类_
+{
+    public class Dean : IMeeting
+    {
+        public void Speech()
+        {
+            Console.WriteLine($"主任正在演讲...");
+        }
+        public string Talk(string topic)
+        {
+            return $"主任正在讨论针对 {topic} ...";
+        }
+    }
+}
